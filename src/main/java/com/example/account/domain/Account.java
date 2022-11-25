853599bf252bed18e47edd5de5f55b95c3c7e50f
dsadara <lim971230@gmail.com>
2022-11-25 13:31:08 +0900

    Account 엔티티 설정

---
+++ src/main/java/com/example/account/domain/Account.java
@@ -0,0 +1,24 @@
+package com.example.account.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+// Entity나 dto에 기본적으로 넣어주는 롬복 어노테이션이라고 함
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+// Entitiy는 일종의 테이블 설정 클래스
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue // id를 pk로 지정
+    private Long id;
+
+    private String accountNumber;
+
+    @Enumerated(EnumType.STRING)  // Enum값을 정수가 아닌 실제 문자열로 DB에 저장
+    private AccountStatus accountStatus;
+}
