49bca39afb906597f75bdff2aba72b977dd3f454
LIM <lim971230@gmail.com>
2022-11-27 22:41:52 +0900

    WebMvcTest로 Controller 테스트

---
+++ src/test/java/com/example/account/controller/AccountControllerTest.java
@@ -0,0 +1,57 @@
+package com.example.account.controller;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.AccountStatus;
+import com.example.account.service.AccountService;
+import com.example.account.service.RedisTestService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+// AccountController에 대한 MVC 테스트 (격리해서)
+@WebMvcTest(AccountController.class)
+class AccountControllerTest {
+
+    // 의존하고 있는 서비스를 모킹
+    // Mock인데 Bean으로 만들어져 자동으로 AccountController에 주입이 된다
+    @MockBean
+    private AccountService accountService;
+
+    @MockBean
+    private RedisTestService redisTestService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void successGetAccount() throws Exception {
+        //given
+        // 이 데이터로 모킹?
+        given(accountService.getAccount(anyLong()))
+                .willReturn(Account.builder()
+                        .accountNumber("3456")
+                        .accountStatus(AccountStatus.IN_USE)
+                        .build());
+        //when
+
+        //then
+        // 특정 URL에 대한 테스트?
+        mockMvc.perform(get ("/account/876"))
+                .andDo(print()) // get시 응답값을 화면에 표시
+                .andExpect(jsonPath("$.accountNumber").value("3456"))
+                .andExpect(jsonPath("$.accountStatus").value("IN_USE"))
+                .andExpect(status().isOk());
+    }
+
+
+}
