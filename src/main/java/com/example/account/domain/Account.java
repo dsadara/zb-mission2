f24b93dcee1b23595677b3d1c061cff49b3a7275
LIM <lim971230@gmail.com>
2022-11-28 16:46:19 +0900

    명세서에 맞게 엔티티 작성

--- src/main/java/com/example/account/domain/Account.java
+++ src/main/java/com/example/account/domain/Account.java
@@ -1,24 +1,40 @@
 package com.example.account.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
-// Entity나 dto에 기본적으로 넣어주는 롬복 어노테이션이라고 함
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-// Entitiy는 일종의 테이블 설정 클래스
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Account {
     @Id // id를 pk로 지정
     @GeneratedValue // pk 자동 생성 기능
     private Long id;
 
+    @ManyToOne
+    private AccountUser accountUser;
     private String accountNumber;
 
     @Enumerated(EnumType.STRING)  // Enum값을 정수가 아닌 실제 문자열로 DB에 저장
     private AccountStatus accountStatus;
+    private Long balance;
+
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+
+    // 모든 테이블에 갖고 있으면 좋은 메타 정보
+    // 어노테이션을 이용해서 자동으로 date 저장
+    @CreatedDate
+    private LocalDateTime createAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
